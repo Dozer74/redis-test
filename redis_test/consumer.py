653a80d9d31139234c729dfff01636dc77cae869
Dozer74 <ev.gorvits@gmail.com>
2021-06-08 12:02:09 +0200

    Use aioredis

--- redis_test/consumer.py
+++ redis_test/consumer.py
@@ -1,9 +1,28 @@
-import redis
+import asyncio
 
-client = redis.Redis(host='localhost', port=6379, db=0)
-ps = client.pubsub()
-ps.subscribe('my-queue')
+import aioredis
 
-for item in ps.listen():
-    if item['type'] == 'message':
-        print(item)
+
+async def reader(channel):
+    async for message in channel.iter():
+        print('Got message: ', message)
+
+
+async def main():
+    redis = await aioredis.create_redis_pool('redis://localhost')
+    channel = await redis.subscribe('my-queue')
+    channel = channel[0]
+
+    loop = asyncio.get_running_loop()
+    loop.create_task(reader(channel))
+
+    try:
+        await asyncio.sleep(1000)
+    except KeyboardInterrupt:
+        pass
+
+    redis.close()
+    await redis.wait_closed()
+
+
+asyncio.run(main())
